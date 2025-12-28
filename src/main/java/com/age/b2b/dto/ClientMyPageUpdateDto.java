98f2e7a67ec4a1140cc54ef467abfc4ad7a2ae37
Shane <dexture0819@naver.com>
2025-12-28 19:04:17 +0900

    고객사 마이페이지 수정

---
+++ src/main/java/com/age/b2b/dto/ClientMyPageUpdateDto.java
@@ -0,0 +1,11 @@
+package com.age.b2b.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClientMyPageUpdateDto {
+    private String email;
+    private String phone;
+}
